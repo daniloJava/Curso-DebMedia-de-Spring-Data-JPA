7bd104e341856bbb727eadbd40db6d7d1770b7b8
Danilo Silva <danilo.manoel_oliveira@hotmail.com>
2016-08-21 00:09:23 -0300

    Criando a primeira Entity

---
+++ Spring-Data-JPA/src/main/java/br/com/CursoSpringDataJPA/entity/Person.java
@@ -0,0 +1,102 @@
+package br.com.CursoSpringDataJPA.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+@Entity
+@Table(
+	name = "PERSONS",
+	indexes = {@Index(columnList = "FIRST_NAME, LAST_NAME", name = "IDX_PERSON_NAME", unique = true)}
+)
+/**Modelo para a a entidade Person
+ * e tambem a criação de uma tabela no banco de dados
+ * 
+ *
+ * @author Danilo Silva
+ *
+ */
+public class Person implements Serializable {
+	//aplicando ID e auto incremento
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_PERSON")
+	private Long id;
+	
+	@Column(name = "PRIMEIRO_NOME", nullable = false, length = 30)
+	private String firstName;
+	
+	@Column(name = "ULTIMO_NOME", nullable = false, length = 60)
+	private String lastName;
+	
+	@Column(name = "IDADE", nullable = false)
+	private Integer age;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	}	
+}
