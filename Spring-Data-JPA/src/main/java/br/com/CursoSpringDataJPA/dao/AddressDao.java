7672d88a4da5fb0c7c1ab3ead2ea9087c57ed65c
Danilo Silva <danilo.manoel_oliveira@hotmail.com>
2016-08-27 21:10:08 -0300

    Atualização de um metodo Dao.

--- Spring-Data-JPA/src/main/java/br/com/CursoSpringDataJPA/dao/AddressDao.java
+++ Spring-Data-JPA/src/main/java/br/com/CursoSpringDataJPA/dao/AddressDao.java
@@ -1,10 +1,23 @@
 package br.com.CursoSpringDataJPA.dao;
 
+import java.util.List;
+
 import br.com.CursoSpringDataJPA.entity.Address;
 
 public class AddressDao extends GenericDao<Address>{
 
 	public AddressDao() {
 		super(Address.class);
+	}
+	
+	/**Prcura uma cidade pelo nome dela
+	 * 
+	 * @param city - recebe o nome da lista
+	 * @return Todos os registro com o nome passado por maremetro.
+	 */
+	public List<Address> findByCity(String city){
+		String sql = "from Address a where a.city like ?";
+		return find(sql, city);
+		
 	}
 }
