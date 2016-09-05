5b7cae9be0a44336683be249f0fa4e9ca3ec19f6
Aluno <Aluno@L111-0301>
2016-09-05 18:13:42 -0300

    Arquivos de configuração Com Spring Boot

---
+++ curso-spring-data/src/main/java/br/com/spring/CursoSpringDataApplication.java
@@ -0,0 +1,12 @@
+package br.com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringDataApplication.class, args);
+	}
+}
