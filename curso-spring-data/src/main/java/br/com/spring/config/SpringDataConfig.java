f6bbc45addffbe97aec1eadf271bdca078510781
Aluno <Aluno@L111-0203>
2016-09-06 18:52:12 -0300

    Fazendo as mesmas configurações
    Fazendo as mesmas configurações do Spring-Data.xml porem com codigo
    JAVA.

---
+++ curso-spring-data/src/main/java/br/com/spring/config/SpringDataConfig.java
@@ -0,0 +1,86 @@
+package br.com.spring.config;
+
+import java.sql.DriverManager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**Classe de configuração baseado em código java
+ * 
+ * @author Danilo Silva
+ *
+ */
+@Configuration //informando que é uma classe de configuração
+@EnableJpaRepositories(basePackages = "br.com.spring.repository")// habilitando o uso do JPA.
+@EnableTransactionManagement//habilitando o uso de transaçoes
+@PropertySource(value = "classpath:application.properties")//habilitar o Spring a ler o Arquivo de propriedades
+public class SpringDataConfig {
+	
+	//Configurando o Data Sources
+	@Value(value = "${jdbc.use}")
+	private String userName; 
+	
+	@Value(value = "${jdbc.pass}")
+	private String password;
+	
+	@Value(value = "${jdbc.driver}")
+	private String driver;
+	
+	@Value(value = "${jdbc.url}")
+	private String url;
+
+	/**Responsavel para o spring trabalhar com Hibernate
+	 * 
+	 * @return
+	 */
+	@Bean
+	public EntityManagerFactory entityManagerFactory(){
+		//<property name="jpaVendorAdapter">
+		HibernateJpaVendorAdapter adapter = new  HibernateJpaVendorAdapter();
+		//<property name="showSql" value="true"></property>
+		adapter.setShowSql(true);
+		//<property name="generateDdl" value="true"></property>
+		adapter.setGenerateDdl(true);
+		
+		
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		//<property name="jpaVendorAdapter">
+		factory.setJpaVendorAdapter(adapter);
+		//<property name="packagesToScan" value="br.com.spring.entity">
+		factory.setPackagesToScan("br.com.spring.entity");
+		//<property name="dataSource" ref="dataSource">
+		factory.setDataSource(dataSource());
+		//finalizando a factory
+		factory.afterPropertiesSet();
+		
+		return factory.getObject();
+	}
+	
+	/**Criação do Bean para a conexão com o Banco de dados.
+	 * 
+	 * @return - Retorno do Objeto configurado para conexão
+	 */
+	@Bean//usa o Id como onome do metodo ou @Bean(name = "DATASOURCE") 
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername(userName);
+		dataSource.setPassword(password);
+		dataSource.setDriverClassName(driver);
+		dataSource.setUrl(url);
+		
+		return dataSource;
+		
+	}
+	
+}
