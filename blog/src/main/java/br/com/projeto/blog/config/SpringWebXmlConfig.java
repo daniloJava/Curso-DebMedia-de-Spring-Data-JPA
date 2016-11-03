34dbbb1c11131aa636098c42bdff7330dfca4ed8
Danilo Silva <danilo.manoel_oliveira@hotmail.com>
2016-11-02 23:41:06 -0200

    Adicionando um Filtro para deixar uma sessão aberta

--- blog/src/main/java/br/com/projeto/blog/config/SpringWebXmlConfig.java
+++ blog/src/main/java/br/com/projeto/blog/config/SpringWebXmlConfig.java
@@ -5,7 +5,9 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -26,6 +28,9 @@ public class SpringWebXmlConfig implements WebApplicationInitializer{
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		// apartir dela que diz para o spring que a aplicação está baseada em anotação.
 		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+		
+		//registra mais uma aplciaçãao
+		servletContext.addListener(new ContextLoaderListener(webContext));
 		
 		webContext.register(SpringMvcConfig.class);//Registra as configuraçãoes
 		webContext.setServletContext(servletContext);//contexto da aplicação são todos os registros de config
@@ -49,6 +54,12 @@ public class SpringWebXmlConfig implements WebApplicationInitializer{
 		enconding.setInitParameter("forceencoding", "true");
 		enconding.addMappingForUrlPatterns(null, true, "/*");// tudo que estiver dentro do barra como UTF-8
 		
+		//para deixar uma sessão aberta ao abrir a pagina até que o processo de renderização da pagina seja concluida.
+		FilterRegistration.Dynamic inViewSession =
+				servletContext.addFilter("Spring OpenEntityManagerInViewFilter", 
+						new OpenEntityManagerInViewFilter());
+		inViewSession.setAsyncSupported(Boolean.TRUE);
+		inViewSession.addMappingForServletNames(null, true, "/*");
 		
 		
 	}
